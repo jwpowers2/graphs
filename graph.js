797be559e6f6a9a10b487706c90eb0460c5cb74d
John Powers <john.powers070@gmail.com>
2018-05-16 14:37:41 -0400

    clean up

--- graph.js
+++ graph.js
@@ -1,11 +1,14 @@
 // breadth graph traversal in JS
- 
+// google maps api info is in ~/Desktop/GoogleMaps
 
 function Graph() {
  
     this.graph = {};
 
     // add an edge to graph
+    // v could be an object which is the computed distance between u and v
+    // need to geolocate an address to lat/long and plug into a function
+
     this.addEdge = function(u,v){
     	if(!this.graph[u]){
     	    this.graph[u] = [];
@@ -15,61 +18,37 @@ function Graph() {
         }
     }
 
-    // BFS
     this.BFS = function(s){
  
-        //Mark all the vertices as not visited
-        //visited = [False] * (len(self.graph))
         var visited = {};
         Object.keys(this.graph).forEach(function(element){
         	visited[element] = false;
         })
 
-        // Create a queue for BFS
         var queue = [];
  
-        // Mark the source node as 
-        // visited and enqueue it
         queue.push(s);
         visited[s] = true;
-        //console.log(visited);
-        
-        //console.log(typeof queue);
+
         
         while (queue.length > 0){
-            
-            //Dequeue a vertex from 
-            //queue and print it
+
             s = queue.shift();
-            console.log(s);
-            console.log(visited);
-            
-            //Get all adjacent vertices of the
-            //dequeued vertex s. If a adjacent
-            //has not been visited, then mark it
-            //visited and enqueue it
-            
+  
             for (let i of Object.keys(this.graph[s])){
-                console.log(this.graph[s][i]);
-                // for each in adjacency array
+                //console.log(this.graph[s][i]);
+                
                 if (visited[this.graph[s][i]] === false){
                     queue.push(this.graph[s][i]);
                     visited[this.graph[s][i]] = true;
-                }
-                
-                
+                }   
             }
-
-            
-
         }
-        
-        
     }
     this.DFS_Util = function(v,visited){
         visited[v] = true;
-        console.log(v);
-        //Recur for all the vertices adjacent to this vertex
+        //console.log(v);
+        
         for (let i of this.graph[v]){
             if (visited[i] === false){
                 this.DFS_Util(i, visited)
@@ -80,22 +59,16 @@ function Graph() {
 
     this.DFS = function(v){
 
-        //Mark all the vertices as not visited
         var visited = {};
         Object.keys(this.graph).forEach(function(element){
         	visited[element] = false;
         })
- 
-        //Call the recursive helper function to print
-        //DFS traversal
+
         this.DFS_Util(v,visited);
 
     }
 } 
-//Driver code
- 
-//Create a graph given in
-//the above diagram
+
 let g = new Graph();
 g.addEdge('Seattle', 'Tacoma');
 g.addEdge('Seattle', 'Renton');
@@ -105,5 +78,5 @@ g.addEdge('Renton', 'Olympia');
 g.addEdge('Olympia', 'Olympia');
 
 
-g.BFS('Seattle');
+//g.BFS('Seattle');
 g.DFS('Seattle');
